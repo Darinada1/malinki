217500355cc0be16f0a5404b9c2043a3a717d944
nikita_ <Pliasnevichn@gmail.com>
2023-05-19 15:18:57 +0200

    add some info to DB

--- tui/main_page/tests.py
+++ tui/main_page/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TuiTest(TestCase):
+    def test_about(self):
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 301)
+
+    def test_contacts(self):
+        response = self.client.get('/contacts')
+        self.assertEqual(response.status_code, 301)
--- tui/main_page/urls.py
+++ tui/main_page/urls.py
@@ -3,11 +3,11 @@ from .views import *
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', MainTui.as_view(), name="home"),
-    path('about/', About.as_view(), name='about'),
-    path('contacts/', Contacts.as_view(), name='contacts'),
-    path('place/', Place.as_view(), name='place'),
-    path('post/<slug:post_slug>/', posts, name='post'),
-    path("category/<slug:cat_slug>", ShowCategory.as_view(), name='category'),
+    path('', cache_page(60*15)(MainTui.as_view()), name="home"),
+    path('about/', cache_page(60*60)(About.as_view()), name='about'),
+    path('contacts/', cache_page(60*60)(Contacts.as_view()), name='contacts'),
+    path('place/', cache_page(60*60)(Place.as_view()), name='place'),
+    path('post/<slug:post_slug>/', cache_page(60*15)(posts), name='post'),
+    path("category/<slug:cat_slug>", cache_page(60*15)(ShowCategory.as_view()), name='category'),
 ]
 
